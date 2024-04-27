aad3bfd38d1b6678eb4f3abb1bd8a05302aa12ef
99Alphapriority <prajneets@gmail.com>
2024-04-26 19:04:47 -0600

    First come first served algorithm completed

---
+++ CPU.c
@@ -0,0 +1,14 @@
+/**
+ * "Virtual" CPU that also maintains track of system time.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include "task.h"
+
+// run this task for the specified time slice
+void run(Task *task, int slice, int tat, int waitTime) {
+    printf("Running task = [%s] [%d] [%d] for %d ms TAT: %d ms waiting time: %d ms.\n",task->name, task->priority, task->burst, slice, tat, waitTime);
+	sleep(slice/1000);
+}
---
+++ schedule_fcfs.c
@@ -0,0 +1,72 @@
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+#include "schedulers.h"
+#include "list.h"
+#include "cpu.h"
+
+struct node *head = NULL;
+struct node *curr = NULL;
+
+void add(char *name, int priority, int burst)
+{
+	Task *task = (Task*) malloc (sizeof(Task));
+	assert(task != NULL);
+
+	task->name = (char*) malloc (sizeof(name));
+	assert(task->name != NULL);
+
+	if(NULL == head)
+	{
+		head = (struct node *) malloc (sizeof(struct node));
+		assert(head != NULL);
+
+		//copy name 
+		strcpy(task->name, name);
+
+		task->priority = priority;
+		task->burst = burst;
+		
+		head->task = task;
+		head->next = NULL;
+
+		curr = head;
+	}
+	else
+	{
+		curr->next = (struct node *) malloc (sizeof(struct node));
+		assert(curr->next != NULL);
+
+		curr = curr->next;
+
+		//copy name
+		strcpy(task->name, name);
+
+		task->priority = priority;
+		task->burst = burst;
+
+		curr->task = task;
+		curr->next = NULL;
+	}
+
+}
+
+void schedule()
+{
+	struct node *Node = head;
+	struct node *temp = Node;
+	int tat = 0;
+	int waitTime = 0;
+
+	while(Node != NULL)
+	{
+		tat = Node->task->burst;
+		run(Node->task, Node->task->burst, tat, waitTime);
+		waitTime += tat;
+		Node = Node->next;
+		free(temp);
+		temp = Node;
+	}		
+}		
